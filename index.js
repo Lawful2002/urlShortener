6733538f3aa5df1328fd4b2f7fcf1c919bccf32a
Harshvardhan Singh <singhharsh5000@gmail.com>
2021-06-20 08:00:24 -0700

    Added Initial Server  Functionality

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const path = require("path")
 const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
+app.set('public', path.join(__dirname, 'public'));
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended: true}));
 
@@ -11,7 +12,6 @@ app.listen(3000, ()=>{
     console.log("Sever started at port 3000");
 })
 
-app.get("/", (req, res)=>{
-    console.log("Home Page");
-    res.send("<h1>Home Page </h1>");
+app.get("/", (req, res)=>{    
+    res.render("home", {page: "Home"});
 })
